38072f6c1c67d8a1178b7f181dbb9fc9381e0d57
Mickael Bobovitch <m.bobovitch@gmail.com>
2024-11-16 02:17:12 +0700

    feat(auth): auth with privy and twitter

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -1,10 +1,45 @@
 "use client";
 
+import { useEffect } from "react";
 import Link from "next/link";
+import { useLoginWithOAuth, usePrivy } from "@privy-io/react-auth";
+import { LogOut, Settings, Wallet } from "lucide-react";
 import { SearchBar } from "@/components/Search";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { NavigationMenu, NavigationMenuItem, NavigationMenuList } from "@/components/ui/navigation-menu";
 
 export const Header = () => {
+  const { ready, logout, user, createWallet } = usePrivy();
+  const { loading, initOAuth } = useLoginWithOAuth();
+
+  const handleInitOAuth = async () => {
+    try {
+      await initOAuth({ provider: "twitter" });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    async function init() {
+      if (user && !user.wallet) {
+        createWallet();
+        return;
+      }
+    }
+
+    init();
+  }, [user, createWallet]);
+
   return (
     <header className="sticky border-b-[1px] top-0 z-40 w-full bg-white dark:border-b-card dark:bg-background">
       <NavigationMenu className="mx-auto">
@@ -27,6 +62,51 @@ export const Header = () => {
           <nav className="items-center justify-center hidden gap-2 grow md:flex">
             <SearchBar className="max-w-[500px]" />
           </nav>
+
+          <div className="gap-2 pl-3 md:flex">
+            {!ready || loading ? (
+              <Button disabled onClick={handleInitOAuth} className="cursor-pointer text-bold bg-card">
+                Loading...
+              </Button>
+            ) : user ? (
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Avatar className="cursor-pointer">
+                    <AvatarImage
+                      src={
+                        user.twitter?.profilePictureUrl
+                          ? user.twitter.profilePictureUrl.replace("normal", "400x400")
+                          : undefined
+                      }
+                      alt={user.twitter?.username ?? "User"}
+                    />
+                    <AvatarFallback>CN</AvatarFallback>
+                  </Avatar>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-56 bg-background">
+                  <DropdownMenuLabel>@{user?.twitter?.username}</DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem>
+                    <Wallet className="w-4 h-4 mr-2" />
+                    <span>0.5 ETH</span>
+                  </DropdownMenuItem>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem className="cursor-pointer">
+                    <Settings className="w-4 h-4 mr-2" />
+                    <span>Settings</span>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem onClick={logout} className="cursor-pointer">
+                    <LogOut className="w-4 h-4 mr-2" />
+                    <span>Log out</span>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            ) : (
+              <Button onClick={handleInitOAuth} className="cursor-pointer text-bold bg-card">
+                Connect with X
+              </Button>
+            )}
+          </div>
         </NavigationMenuList>
       </NavigationMenu>
     </header>
